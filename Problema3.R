f606739f2f2b9b424f85fb76945b9be7f2d3d330
bruno.quero <bruno.quero@auleseebe.upc.edu>
2024-09-12 12:07:03 +0200

    problema 3

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,6 @@
 #Codigo para problema 3
+x<-c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5) #creamos un vector con el comando de concatenar c.
+sum(x)
+y<-x[-c(21,2,26)] #quitamos los numerose en las posiciones 21,2,26 al vector c
+sum(exp(x))-sum(exp(y))
+y[c(19,24)] #truquem els valors de les posicions 19,24 per compararlos 
